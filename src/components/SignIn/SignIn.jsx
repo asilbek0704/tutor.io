9bc3f0004566b8bfbb12313dc4da4da1adcf68df
volta-iut <voltaiut@gmail.com>
2024-01-28 13:28:21 +0500

    some commit

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,43 +1,45 @@
-
 import { Link } from "react-router-dom";
 import { Container } from "../../views/Container/Container";
 import s from "./SignIn.module.scss";
 
-export const SignIn = () => (
-  <Container className={s.signInContainer}>
-    <div className={s.content}>
-      <h2 className={s.title}>Вход в аккаунт</h2>
+export const SignIn = () => {
+  
+  return (
+    <Container className={s.signInContainer}>
+      <div className={s.content}>
+        <h2 className={s.title}>Вход в аккаунт</h2>
 
-      <form className={s.form} action="">
-        <label className={s.label} htmlFor="signIn_username">
-          Юзернейм
-        </label>
-        <input
-          className={s.input}
-          type="text"
-          id="signIn_username"
-          placeholder="Введите юзернейм"
-        />
+        <form className={s.form} action="">
+          <label className={s.label} htmlFor="signIn_username">
+            Юзернейм
+          </label>
+          <input
+            className={s.input}
+            type="text"
+            id="signIn_username"
+            placeholder="Введите юзернейм"
+          />
 
-        <label className={s.label} htmlFor="signIn_password">
-          Пароль
-        </label>
-        <input
-          className={s.input}
-          type="password"
-          id="signIn_password"
-          placeholder="Введите пароль"
-        />
+          <label className={s.label} htmlFor="signIn_password">
+            Пароль
+          </label>
+          <input
+            className={s.input}
+            type="password"
+            id="signIn_password"
+            placeholder="Введите пароль"
+          />
 
-        <button className={s.submit}>Войти</button>
-      </form>
+          <Link to="/" className={s.submit}>Войти</Link>
+        </form>
 
-      <p className={s.text}>
-        Еще нет аккаунта?&nbsp;
-        <Link to="/signup" className={s.link}>
-          Создать аккаунт
-        </Link>
-      </p>
-    </div>
-  </Container>
-);
+        <p className={s.text}>
+          Еще нет аккаунта?&nbsp;
+          <Link to="/signup" className={s.link}>
+            Создать аккаунт
+          </Link>
+        </p>
+      </div>
+    </Container>
+  );
+};
